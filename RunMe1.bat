2cffee74fed74abc83cd9620dcc6983731b22d93
vijnasu <hemanthjois@gmail.com>
2017-11-02 12:52:39 +0530

    Windows and Linux Setup instructions

--- RunMe1.bat
+++
@@ -1,6 +0,0 @@
-"%cd%\Drivers\Chipset_Win7_8.1_10_10.1.1.42/SetupChipset.exe"
-"%cd%\Drivers\GFX_Win7_8.1_10_64_15.40.34.4624\SetupChipset.exe"
-"%cd%\Drivers\ME_Corporate_Win7_8.1_10_11.0.6.1194\MEI-Only Installer MSI\MEISetup.exe"
-"%cd%\Drivers\LAN_WIN10_64_22.1.exe"
-"%cd%\Drivers\AUD_Win7_8.1_10_6.0.1.7982\7982_PG453_Win10_TH_RS_Win8.1_Win8_Win7_WHQL\Setup.exe"
-Intel_Media_SDK_2016_R2.msi
